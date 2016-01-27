b4e4c43cd1b4a87d484786aa7d44dab4d321d29c
Jason Whittle <jason.whittle@gmail.com>
2016-01-27 17:16:17 -0500

    Web starter from http://start.spring.io

---
+++ src/main/java/co/masslab/ShibaAuth0Application.java
@@ -0,0 +1,12 @@
+package co.masslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShibaAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShibaAuth0Application.class, args);
+	}
+}
