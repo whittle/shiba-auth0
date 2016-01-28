805ebc9f0505bbc3478efba12b47e460f016d2d2
Jason Whittle <jason.whittle@gmail.com>
2016-01-28 13:08:01 -0500

    Follow the instructions at https://auth0.com/docs/server-apis/java-spring-security
    N.B. Tests now fail, app crashes on start.

--- src/main/java/co/masslab/ShibaAuth0Application.java
+++ src/main/java/co/masslab/ShibaAuth0Application.java
@@ -2,7 +2,12 @@ package co.masslab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.*;
 
+@Configuration
+@ComponentScan("com.auth0")
+@ImportResource("classpath:auth0-security-context.xml")
+@PropertySource("classpath:auth0.properties")
 @SpringBootApplication
 public class ShibaAuth0Application {
 
